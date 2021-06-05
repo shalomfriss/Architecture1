f5d13d499542f25eb15cf11cbf93c513e3dd3041
shalomfriss <shalomfriss@gmail.com>
2021-06-05 11:56:17 -0500

    Initial Commit

---
+++ Architecture1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Architecture1
+//
+//  Created by Shalom Friss on 6/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
